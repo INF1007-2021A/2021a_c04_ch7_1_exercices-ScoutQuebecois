55b48d88bd4b99e7d8869768040a941e8b5a9562
ScoutQuebecois <eloivl13@gmail.com>
2021-10-27 12:52:39 -0400

    premier push

--- exercice.py
+++ exercice.py
@@ -3,12 +3,25 @@
 
 # TODO: Importez vos modules ici
 
+from turtle import Turtle
 
 
 # TODO: Définissez vos fonction ici
 
+def recur_tree(tur,i):
+    
+    if (i>10):
+        tur.fd(i)
+        tur.left(i/2)
+        recur_tree(tur,2/3 * i)
+        tur.right(i)
+        recur_tree(tur,2/3 * i)
+        tur.left(i/2)
+        tur.bk(i)
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    tur = Turtle()
+    tur.left(90)
+    recur_tree(tur,100)
     pass
